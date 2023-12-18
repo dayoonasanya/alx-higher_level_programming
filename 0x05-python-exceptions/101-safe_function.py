0659efc866d68d790a871e1927da9de85419dadc
dayoonasanya <aonasanya@mail.dccc.edu>
2023-12-18 10:19:20 -0800

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    p = 0
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception('Too far')
+            else:
+                p += a ** b / x
+        except Exception:
+            p = b + a
+            break
+    return (p)
