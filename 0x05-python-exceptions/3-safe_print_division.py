e4e2de849ecce3bc59873b857119b3d52a63e38e
dayoonasanya <aonasanya@mail.dccc.edu>
2023-12-18 09:54:13 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Returns the result of the ivision a/b"""
+    try:
+        d = a / b
+    except (TypeError, ZeroDivisionError):
+        d = None
+    finally:
+        print("Inside result: {}".format(d))
+    return (d)
