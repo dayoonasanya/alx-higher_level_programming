b0fe6cc65511103da5f2d7abecbe77454ae19fcd
dayoonasanya <aonasanya@mail.dccc.edu>
2023-12-19 10:03:46 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class Square that defines a square object
+    """
+    def _init_(self, size):
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = sie
